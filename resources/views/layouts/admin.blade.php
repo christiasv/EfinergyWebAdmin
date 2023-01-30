b9b23ce58db40686e2d0c8d158e5c5875d893b67
Christian Sánchez <christiasv@gmail.com>
2023-01-29 22:45:41 -0500

    29/01/2023 * Se creo el layout para la web principal.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,8 +13,8 @@
     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/ico/apple-icon-76.png')}}">
     <link rel="apple-touch-icon" sizes="120x120" href="{{asset('img/ico/apple-icon-120.png')}}">
     <link rel="apple-touch-icon" sizes="152x152" href="{{asset('img/ico/apple-icon-152.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/ico/favicon.ico')}}">
-    <link rel="shortcut icon" type="image/png" href="{{asset('img/ico/favicon-32.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/ico/isotipo.ico')}}">
+    <link rel="shortcut icon" type="image/png" href="{{asset('img/ico/isotipo.png')}}">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Efinergy - Consultora del Perú</title>
+    <link rel="shortcut icon" href="img/icon/isotipo.png" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/meanmenu.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/et-line-icon.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v5.0&appId=319117778792165&autoLogAppEvents=1"></script>
+    <div class="content-wrapper"><!--Statistics cards Starts-->
+
+        <!--Contenido-->
+        @yield('contenido-web')
+        <!--Fin contenido-->
+
+    </div>
+
+    <script src="js/vendor/jquery-1.12.0.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/jquery.meanmenu.js"></script>
+    <script src="js/jquery.magnific-popup.js"></script>
+    <script src="js/ajax-mail.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.mb.YTPlayer.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
